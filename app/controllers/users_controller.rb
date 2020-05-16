373f5d061bed219a37bf181e1a466889f62cadfa
shinya-jp <shinya0733@gmail.com>
2020-05-16 02:50:20 +0000

    新規登録画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:email)
+		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
 	end
 end
