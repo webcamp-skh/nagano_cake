2afd67980f477c8b549db59e3d9f58c80467b77a
shinya-jp <shinya0733@gmail.com>
2020-05-16 11:09:52 +0000

    user基本実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def quit
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
