4ea5bd8fffc05b101eeb5d63a9c38baf4b5e5a93
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-05-17 02:24:36 +0000

    Add 注文情報入力画面一部作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,15 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
+    if params[:user][:user_address] == 0
+      @order.ordered_postal_code = current_user.postal_code
+      @order.ordered_address = current_user.address
+      @order.ordered_receiver_name = "#{current_user.last_name}#{current_user.first_name}"
+    elsif params[:user][:user_address] == 1
+
+    elsif params[:user][:user_address] == 2
+
+    end
   end
 
   def confirm
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit: 0, bank: 1 }
 end
