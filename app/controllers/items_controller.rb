22871342512a2cef72b78425238aea142b9d1cfe
kazuya-sukigara <kazuyahahaha@icloud.com>
2020-05-17 02:09:44 +0000

    item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,24 @@
+class ItemsController < ApplicationController
+
+  def index
+  	  @item = Item.new
+  	  @items = Item.all
+  end
+
+  def show
+  	  @item = Item.find(params[:id])
+  end
+
+  def create
+      @item = Item.new(item_params)
+      @items = Item.all
+      @item.save 
+      redirect_to @item     
+  end
+
+  private
+  def item_params
+  	params.require(:item).permit(:name, :description, :image_id, :status, :price)
+  end
+
+end
