5397d920c735fe24d5b41b3c50d92936fb9ca538
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-05-17 07:16:12 +0000

    Add 注文情報入力画面作成（view大枠完成）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,30 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
-    if params[:user][:user_address] == 0
-      @order.ordered_postal_code = current_user.postal_code
-      @order.ordered_address = current_user.address
-      @order.ordered_receiver_name = "#{current_user.last_name}#{current_user.first_name}"
-    elsif params[:user][:user_address] == 1
-
-    elsif params[:user][:user_address] == 2
-
-    end
   end
 
   def confirm
+    @order = Order.new
+    @order.billing_amount = @order.
+    @order.payment_method =
+    if params[:address_id] == 0
+      @order.ordered_postal_code = current_user.postal_code
+      @order.ordered_address = current_user.address
+      @order.ordered_receiver_name = "#{current_user.last_name}#{current_user.first_name}"
+    elsif params[:address_id] == 1
+      @address = current_user.address.find(params[:id])
+      @order.ordered_postal_code = @address.postal_code
+      @order.ordered_address = @address.address
+      @order.ordered_receiver_name = @address.receiver_name
+    elsif params[:address_id] == 2
+      @address = current_user.addresses.new(address_params)
+      @order.ordered_postal_code = @address.postal_code
+      @order.ordered_address = @address.address
+      @order.ordered_receiver_name = @address.receiver_name
+    end
+  end
+
+  def create
   end
 
   def thanks
@@ -22,5 +34,10 @@ class OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :receiver_name)
   end
 end
