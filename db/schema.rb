683d860f85264237747e7ec93d9e5e9183d942ad
shinya-jp <shinya0733@gmail.com>
2020-05-18 01:31:06 +0000

    addressの追加途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_130238) do
+ActiveRecord::Schema.define(version: 2020_05_17_083840) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "postal_code"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_130238) do
     t.string "receiver_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "cart_items", force: :cascade do |t|
