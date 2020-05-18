f0fc09b04d8a37ea302ce5f0dfb83ca131828a3b
shinya-jp <shinya0733@gmail.com>
2020-05-18 02:27:29 +0000

    配送先一覧ボタン作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_17_141206) do
 
   create_table "addresses", force: :cascade do |t|
