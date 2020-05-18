f29a7683ad4733623e427019d4f73782d577cf40
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-05-18 09:53:38 +0000

    Add 注文確認画面作成（途中）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,31 +4,40 @@ class OrdersController < ApplicationController
   end
 
   def confirm
-    cart_items = current_user.cart_items.pluck(:item_id) #カート内のitem_idを全て抽出
-    @order_item = OrderItem.new
-    @order_item.item_id = cart_items
-
-    @order = Order.new
-    #@order.billing_amount = @order.
-    #@order.payment_method =
+    @order = current_user.orders.new(order_params)
+    @order.shipping = 800
+    #sum_ordered_price =   ←商品小計
+    #@order.billing_amount = @order.shipping + sum_ordered_price
     if params[:address_id] == 0
       @order.ordered_postal_code = current_user.postal_code
       @order.ordered_address = current_user.address
       @order.ordered_receiver_name = "#{current_user.last_name}#{current_user.first_name}"
     elsif params[:address_id] == 1
-      @address = current_user.address.find(params[:id])
+      @address = Address.find(params[:id])
       @order.ordered_postal_code = @address.postal_code
       @order.ordered_address = @address.address
       @order.ordered_receiver_name = @address.receiver_name
     elsif params[:address_id] == 2
       @address = current_user.addresses.new(address_params)
+      @address.save
       @order.ordered_postal_code = @address.postal_code
       @order.ordered_address = @address.address
       @order.ordered_receiver_name = @address.receiver_name
     end
+
+    #cart_items = current_user.cart_items.pluck(:item_id) #カート内のitem_idを全て抽出
+    #@order.order_items = OrderItem.new
+    #@order.order_items.item_id = cart_items
+    #@order_items = OrderItem.all
+
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    cart_items = CartItem.where(user_id: current_user.id)
+    cart_items.destroy
+    redirect_to 'thanks'
   end
 
   def thanks
@@ -41,6 +50,12 @@ class OrdersController < ApplicationController
   end
 
   private
+  def order_params
+    params.require(:order).permit(
+      :user_id, :payment_method, :billing_amount, :ordered_postal_code, :ordered_address, :ordered_receiver_name, 
+      order_items_attributes: [:item_id, :item_count, :ordered_price])
+  end
+
   def address_params
     params.require(:address).permit(:postal_code, :address, :receiver_name)
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   has_many :orders, dependent: :destroy
   belongs_to :user
   enum payment_method: { credit: 0, bank: 1 }
+  enum order_status: { pending: 0, confirm: 1, making: 2, ready: 3, complete: 4 }
 end
