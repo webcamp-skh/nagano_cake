08c31bf1492bc7b678d2c65319da5f74a6955916
vagrant <vagrant@localhost.localdomain>
2020-05-19 14:52:04 +0000

    Add 注文確認機能（途中）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,10 +4,18 @@ class OrdersController < ApplicationController
   end
 
   def confirm
-    @order = current_user.orders.new(order_params)
-    @order.shipping = 800
-    #sum_ordered_price =   ←商品小計
-    #@order.billing_amount = @order.shipping + sum_ordered_price
+    @order = current_user.orders.new
+    @order.payment_method = params[:payment_method]
+
+    @order.shipping = 800 #送料
+
+    @item_total_price = 0 #商品合計金額(税込)
+    current_user.cart_items.each do |cart_item|
+      subtotal_price = cart_item.item.price * cart_item.item_count * 110 / 100.0 #小計(税込)
+      @item_total_price += subtotal_price
+    end
+    @order.billing_amount = item_total_price + @order.shipping #請求金額(税込)=商品合計金額+送料
+
     if params[:address_id] == 0
       @order.ordered_postal_code = current_user.postal_code
       @order.ordered_address = current_user.address
@@ -18,17 +26,11 @@ class OrdersController < ApplicationController
       @order.ordered_address = @address.address
       @order.ordered_receiver_name = @address.receiver_name
     elsif params[:address_id] == 2
-      @address = current_user.addresses.new(address_params)
-      @address.save
-      @order.ordered_postal_code = @address.postal_code
-      @order.ordered_address = @address.address
-      @order.ordered_receiver_name = @address.receiver_name
+      @order.ordered_postal_code = params[:postal_code]
+      @order.ordered_address = params[:address]
+      @order.ordered_receiver_name = params[:receiver_name]
     end
 
-    #cart_items = current_user.cart_items.pluck(:item_id) #カート内のitem_idを全て抽出
-    #@order.order_items = OrderItem.new
-    #@order.order_items.item_id = cart_items
-    #@order_items = OrderItem.all
 
   end
 
@@ -36,8 +38,17 @@ class OrdersController < ApplicationController
     order = Order.new(order_params)
     order.save
 
+    current_user.cart_items.each do |cart_item|
+      order_item = OederItem.new
+      order_item.item_id = cart_item.item_id
+      order_item.order_id = order.id
+      order_item.item_count = cart_item.item_count
+      order_item.ordered_price = cart_item.item.price * 110 / 100.0 #税込
+      order_item.save
+    end
 
     current_user.cart_items.destroy_all
+
     redirect_to 'thanks'
   end
 
@@ -53,8 +64,7 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order).permit(
-      :user_id, :payment_method, :billing_amount, :ordered_postal_code, :ordered_address, :ordered_receiver_name, 
-      order_items_attributes: [:item_id, :item_count, :ordered_price])
+      :user_id, :payment_method, :billing_amount, :ordered_postal_code, :ordered_address, :ordered_receiver_name)
   end
 
   def address_params
