53fdb35e50482a1d1be8a20f660fa3f6fb6b1506
shinya-jp <shinya0733@gmail.com>
2020-05-21 10:06:49 +0000

    adminUser機能完了

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,21 @@
 class Admin::UsersController < ApplicationController
 
 	def index
-		@user = User.all
+		@users = User.all
 	end
 
 	def show
-		
+		@user = User.find(params[:id])
 	end
 
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		redirect_to admin_users_path
 	end
 
 	private
