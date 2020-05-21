68382e2a664b740ff1fb1b3877f5f6c2640a166f
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-05-21 01:06:00 +0000

    first commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,19 @@
 class Admin::OrdersController < ApplicationController
-
 	def top
 	end
+
+  def index
+  end
+
+  def user_index
+  end
+
+  def today_index
+  end
+
+  def show
+  end
+
+  def update
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,16 @@ class ApplicationController < ActionController::Base
 	    when Admin
 	      admin_root_path
 	    end
- 	 end
+ 	end
 
-	def after_sign_out_path_for(resouce)
-		 if Admin
-		     new_admin_session_path
-		 else
-		    root_path
-	     end
-	 end
+	def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
 
 	private
 	def configure_permitted_parameters
