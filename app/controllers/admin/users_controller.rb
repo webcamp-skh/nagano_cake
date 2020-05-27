6919105b9d74cda65295fede387d59ff43659c5d
hamada_hiroshi <r.m.c.f.789@gmail.com>
2020-05-27 07:35:50 +0000

    search機能修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,7 +18,7 @@ class Admin::UsersController < ApplicationController
 	def update
 		@user = User.find(params[:id])
 		@user.update(user_params)
-		redirect_to admin_users_path
+		redirect_to admin_user_path(@user)
 	end
 
 	def set_serch
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   def index
   	  @item = Item.new
-  	  @items = Item.page(params[:page]).reverse_order
+  	  @items = Item.where(status: true).page(params[:page]).reverse_order
       @genres = Genre.where(status: true)
 
   end
