a1696b644bc3507df010fa6ecc61f744a4ec996b
shinya-jp <shinya0733@gmail.com>
2020-05-27 02:20:58 +0000

    devise日本語

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,3 +57,11 @@ $(function() {
     });
   });
 });
+
+$('#item_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#preview").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @item1 = Item.find(4)
     @item2 = Item.find(5)
     @item3 = Item.find(6)
-    @item4 = Item.find(7)
+    @item4 = Item.find(8)
   end
 
   def about
